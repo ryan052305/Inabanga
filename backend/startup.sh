c4fa65ccb1ec9da982f88e9e3d8eb9df4b2834d9
Ryan <allanicryan@gmail.com>
2025-11-02 22:30:10 +0800

    some tweaks on backend

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "✅ Starting FastAPI server with Playwright support..."
 playwright install --with-deps chromium
-uvicorn app.main:app --host 0.0.0.0 --port 8000
+uvicorn app.main:app --host 0.0.0.0 --port $PORT
