e259fd0139b857f8116ea840fdaa0fc59f4a5041
Ryan <allanicryan@gmail.com>
2025-11-02 13:30:29 +0800

    Update auth.config.ts

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: process.env.CLERK_FRONTEND_API_URL,
+      domain: process.env.CLERK_FRONTEND_API_URL || "https://clerk.inabanga.com",
       applicationID: 'convex',
     },
   ],
